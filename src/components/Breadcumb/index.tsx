b66eb01dfb9b7c8409188ad0f391c3f7a21b18c1
Nizar Fazari <nexsel32@gmail.com>
2024-07-17 23:56:59 +0700

    change into icon phospor arrow left

--- src/components/Breadcumb/index.tsx
+++ src/components/Breadcumb/index.tsx
@@ -1,3 +1,4 @@
+import { ArrowLeft } from '@phosphor-icons/react';
 import React from 'react';
 
 type Props = {
@@ -7,8 +8,8 @@ type Props = {
 
 const Breadcrumb: React.FC<Props> = ({ title, subtitle }) => {
   return (
-    <div className="flex gap-4">
-      <img className="" src="/images/icons/ArrowLeft.svg" alt="Arrow left" />
+    <div className="flex gap-4 ">
+      <ArrowLeft size={27} weight="bold" color='#343330' className='mt-[5px]' />
       <div className="font-bold">
         <h5 className="text-neutral-400 text-heading-5  ">{title}</h5>
         <p className="text-neutral-300 text-body-large ">{subtitle}</p>

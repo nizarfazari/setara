7d4e707e52ae60cdfb66aebebd13eed07cb39bd7
Nizar Fazari <nexsel32@gmail.com>
2024-07-17 23:49:53 +0700

    change size on icons navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -31,19 +31,19 @@ const Navbar = () => {
                 <img src="/images/logo.png" alt="Logo" className="h-12" />
                 <ul className='hidden md:flex gap-6 text-white'>
                     <a href="#beranda" className="gap-[6px] flex items-center">
-                        <House size={25} color='#fff' weight="fill" />
+                        <House size={20} color='#fff' weight="fill" />
                         <p>Beranda</p>
                     </a>
                     <a href="#mutasi" className="gap-[6px] flex items-center">
-                        <Receipt size={25} color='#fff' weight="fill" />
+                        <Receipt size={20} color='#fff' weight="fill" />
                         <p>Mutasi</p>
                     </a>
                     <a href="#notif" className="gap-[6px] flex items-center">
-                        <Bell size={25} color='#fff' weight="fill" />
+                        <Bell size={20} color='#fff' weight="fill" />
                         <p>Notifikasi</p>
                     </a>
                     <a href="#profile" className="gap-[6px] flex items-center">
-                        <User size={25} color='#fff' weight="fill" />
+                        <User size={20} color='#fff' weight="fill" />
                         <p>Profile</p>
                     </a>
                 </ul>
@@ -57,19 +57,19 @@ const Navbar = () => {
                 : <></>}
             <ul className={`fixed top-20 font-bold h-full text-primary-100 text-nunito bg-white z-[11] ${nav ? 'right-0 w-[60%] duration-500' : 'duration-500 right-[-400px]'}`}>
                 <a href="#beranda" className="p-4 flex gap-2 items-center">
-                    <House size={25} color='#115DA9' weight="fill" />
+                    <House size={20} color='#115DA9' weight="fill" />
                     <p>Beranda</p>
                 </a>
                 <a href="#mutasi" className="p-4 flex gap-2 items-center">
-                    <Receipt size={25} color='#115DA9' weight="fill" />
+                    <Receipt size={20} color='#115DA9' weight="fill" />
                     <p>Mutasi</p>
                 </a>
                 <a href="#notif" className="p-4 flex gap-2 items-center">
-                    <Bell size={25} color='#115DA9' weight="fill" />
+                    <Bell size={20} color='#115DA9' weight="fill" />
                     <p>Notifikasi</p>
                 </a>
                 <a href="#profile" className="p-4 flex gap-2 items-center">
-                    <User size={25} color='#115DA9' weight="fill" />
+                    <User size={20} color='#115DA9' weight="fill" />
                     <p>Profile</p>
                 </a>
             </ul>

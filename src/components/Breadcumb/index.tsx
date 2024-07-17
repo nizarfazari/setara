abf8d8b65a854f22b62c34c24c328a9c7c59dcce
Nizar Fazari <nexsel32@gmail.com>
2024-07-17 23:39:04 +0700

    adjust navbar,breadcrumb and responsive navbar

--- src/components/Breadcumb/index.tsx
+++ src/components/Breadcumb/index.tsx
@@ -7,11 +7,11 @@ type Props = {
 
 const Breadcrumb: React.FC<Props> = ({ title, subtitle }) => {
   return (
-    <div className="container mx-auto px-8 flex">
+    <div className="flex gap-4">
       <img className="" src="/images/icons/ArrowLeft.svg" alt="Arrow left" />
-      <div className="container mt-6">
-        <h5 className="text-neutral-400 text-heading-5 text-nunito font-bold">{title}</h5>
-        <p className="text-neutral-300 text-body-large text-nunito font-bold">{subtitle}</p>
+      <div className="font-bold">
+        <h5 className="text-neutral-400 text-heading-5  ">{title}</h5>
+        <p className="text-neutral-300 text-body-large ">{subtitle}</p>
       </div>
     </div>
   );

d8e365bcf7cd47a0f0f547993027b46e14e2ea9b
fitriandriy <202410102062@mail.unej.ac.id>
2024-07-23 18:40:08 +0700

    fix: margin top

--- src/pages/TransaksiGagal/index.tsx
+++ src/pages/TransaksiGagal/index.tsx
@@ -9,14 +9,14 @@ const TransactionFailed = () => {
     navigate("/");
   };
   return (
-    <div className="h-screen  flex justify-center items-center container">
-      <div className="text-center md:w-[600px] mb-[100px]">
+    <div className="flex justify-center items-center container mt-10">
+      <div className="text-center md:w-[600px] mb-[50px]">
         <h1 className=" text-heading-6 md:text-heading-5 font-bold">Oops, Transaksi Gagal :(</h1>
-        <p className="font-bold text-neutral-300 mb-8">
+        <p className="font-bold text-[14px] md:text-[16px] text-neutral-300 my-2">
           Transaksi anda gagal kami proses karena beberapa masalah teknis. Mohon
           coba kembali dalam beberapa saat.
         </p>
-        <img src={image} className="mx-auto w-[273px] h-[300px]" />
+        <img src={image} className="mx-auto w-[240px] h-[240px]" />
         <button onClick={handleHome} className="bg-primary-300 w-full p-3 rounded-xl text-primary-100 font-bold mt-7">Kembali ke HomePage</button>
       </div>
     </div>

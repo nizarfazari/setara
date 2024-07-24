812a343c9c7590fb4032c290656c6c4ead138a20
fitriandriy <202410102062@mail.unej.ac.id>
2024-07-24 20:33:05 +0700

    feat: slicing confirmation page

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import Notifikasi from './pages/Notifikasi';
 import TransferBCA from "./pages/BCA";
 import AmountTopUpBCA from "./pages/BCA/AmountTopUp";
 import DestinationNumberBCA from "./pages/BCA/DestinationNumber/DestinationNumber";
+import KonfirmasiBCA from './pages/BCA/Confirmation';
 import ConfirmationPINBCA from "./pages/BCA/ConfirmationPin";
 import PlainLayout from "./layouts/PlainLayout";
 
@@ -25,6 +26,7 @@ import TransferWallet from "./pages/E-Wallet";
 import DestinationNumberPage from "./pages/E-Wallet/DestinationNumber/DestinationNumber";
 import AmountTopUpPage from "./pages/E-Wallet/AmountTopUp";
 import ConfirmationPIN from "./pages/E-Wallet/ConfirmationPin";
+import Konfirmasi from './pages/E-Wallet/Confirmation';
 
 function App() {
   return (
@@ -42,12 +44,14 @@ function App() {
             <Route index element={<TransferWallet />} />
             <Route path="/e-wallet/:slug" element={<DestinationNumberPage />} />
             <Route path="/e-wallet/:slug/nomor-tujuan" element={<AmountTopUpPage />} />
+            <Route path="/e-wallet/:slug/tinjau" element={<Konfirmasi />} />
             <Route path="/e-wallet/:slug/konfirmasi" element={<ConfirmationPIN />} />
           </Route>
           <Route path="/bca" element={<PlainLayout />}>
             <Route index element={<TransferBCA />} />
             <Route path="/bca/:slug" element={<DestinationNumberBCA />} />
             <Route path="/bca/:slug/nomor-tujuan" element={<AmountTopUpBCA />} />
+            <Route path="/bca/:slug/tinjau" element={<KonfirmasiBCA />} />
             <Route path="/bca/:slug/konfirmasi" element={<ConfirmationPINBCA />} />
           </Route>
           <Route path="/login" element={<Login />} />

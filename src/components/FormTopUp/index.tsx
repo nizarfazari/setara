cfca600ee5c29a6e471f584d5bb1e34ff8d101c0
Nizar Fazari <nexsel32@gmail.com>
2024-08-14 00:14:32 +0700

    adjust rule and validator on form top up

--- src/components/FormTopUp/index.tsx
+++ src/components/FormTopUp/index.tsx
@@ -1,6 +1,8 @@
 import { Button, Form, FormProps, Input, InputNumber } from "antd";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../../hooks/useAuth";
+import { useFetchData } from "../../hooks/useFetchData";
+import { BalanceData } from "../../types/E-Wallet";
 
 type TFormTopUp = {
   amount: number;
@@ -13,10 +15,12 @@ type PropsFormTopup = {
 }
 
 const FormTopUp: React.FC<PropsFormTopup> = ({ pathUrl, isTfBa = false }) => {
-  const { setProcessTransaction, transactions } = useAuth()
+  const { setProcessTransaction, transactions, user } = useAuth()
   const navigate = useNavigate();
 
 
+  const { data } = useFetchData<BalanceData>('/user/getBalance', user?.token)
+  
   const onFinish: FormProps<TFormTopUp>["onFinish"] = (values) => {
     setProcessTransaction({
       nominal: values.amount.toString(),
@@ -39,17 +43,22 @@ const FormTopUp: React.FC<PropsFormTopup> = ({ pathUrl, isTfBa = false }) => {
     >
       <Form.Item
         name="amount"
-        label="Nominal Top Up"
+        label="Nominal Transfer"
         rules={[
-          { required: true, message: "Nominal Tidak Boleh Kosong" },
-          () => ({
+          {
             validator(_, value) {
-              if (isTfBa ? value >= 1 : value >= 10000) {
-                return Promise.resolve();
+              if (!value) {
+                return Promise.reject(new Error("Nominal Tidak Boleh Kosong"));
               }
-              return Promise.reject(new Error(isTfBa ? "Minimum transfer adalah 1, mohon isikan kembali" : "Minimum transfer adalah 10000, mohon isikan kembali"));
+              if (isTfBa ? value < 1 : value < 10000) {
+                return Promise.reject(new Error(isTfBa ? "Minimum transfer adalah 1, mohon isikan kembali" : "Minimum transfer adalah 10.000, mohon isikan kembali"));
+              }
+              if (data?.balance !== undefined && data.balance < value) {
+                return Promise.reject(new Error("Saldo tidak mencukupi"));
+              }
+              return Promise.resolve();
             },
-          }),
+          },
         ]}
         required
       >
